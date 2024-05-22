654b07f6fd6b61f8b921b3369df6f3332d5b3491
volemy <minayobenevolence0@gmail.com>
2024-05-22 06:27:58 +0000

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""BasicCache that inherits from BaseCaching"""
+"""
+Basic dictionary
+"""
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    BasicCache defines:
+    """Inherits from BaseCaching and is a caching system:
+       -use self.cache_data - dictionary from the parent class BaseCaching
+       -This caching system doesn’t have limit
     """
     def __init__(self) -> None:
         """BaseCaching Caching System"""
         super().__init__()
 
     def put(self, key, item):
-        """Add an item in the cache."""
-        if key is None and item is None:
-            pass
-        self.cache_data[key] = item
+        """Put function definition"""
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """
-        (Get and item in cache)Must return the value in self.cache_data
-        linked to key.If key is None or if the key doesn’t exist
-        in self.cache_data, return None.
-        """
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        return self.cache_data.get(key)
+        """Get function definition"""
+        if key is not None:
+            for k, value in self.cache_data.items():
+                if k == key:
+                    return value
+        return None
